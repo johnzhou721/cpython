017c7b53d150470a5363ad8e782f8b614115fdba
John Zhou <john-xyz@outlook.com>
2025-05-01 21:49:34 -0500

    more fixups

--- Lib/test/test_importlib/extension/test_loader.py
+++ Lib/test/test_importlib/extension/test_loader.py
@@ -1,4 +1,4 @@
-from test.support import is_apple_mobile
+from test.support import is_apple_mobile, is_mac_catalyst
 from test.test_importlib import abc, util
 
 machinery = util.import_importlib('importlib.machinery')
@@ -110,7 +110,7 @@ class SinglePhaseExtensionModuleTests(abc.LoaderTests):
 
         # Apple extensions must be distributed as frameworks. This requires
         # a specialist loader.
-        if is_apple_mobile:
+        if is_apple_mobile and not is_mac_catalyst:
             self.LoaderClass = self.machinery.AppleFrameworkLoader
         else:
             self.LoaderClass = self.machinery.ExtensionFileLoader
@@ -198,7 +198,7 @@ class MultiPhaseExtensionModuleTests(abc.LoaderTests):
 
         # Apple extensions must be distributed as frameworks. This requires
         # a specialist loader.
-        if is_apple_mobile:
+        if is_apple_mobile and not is_mac_catalyst:
             self.LoaderClass = self.machinery.AppleFrameworkLoader
         else:
             self.LoaderClass = self.machinery.ExtensionFileLoader
--- Lib/test/test_importlib/test_util.py
+++ Lib/test/test_importlib/test_util.py
@@ -714,7 +714,7 @@ class IncompatibleExtensionModuleRestrictionsTests(unittest.TestCase):
     def test_incomplete_multi_phase_init_module(self):
         # Apple extensions must be distributed as frameworks. This requires
         # a specialist loader.
-        if support.is_apple_mobile:
+        if support.is_apple_mobile and not support.is_mac_catalyst:
             loader = "AppleFrameworkLoader"
         else:
             loader = "ExtensionFileLoader"
